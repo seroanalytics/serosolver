0d3abe30e0945b23f59bada9c4be73730d088344
James Hay <jameshay218@gmail.com>
2017-12-08 12:05:44 +0000

    Revert "Revert "making changes to data cleaning for monthly""
    This reverts commit 92d52e2e6e6c3ae554b5f798c66a02ba6a4e6ad9.

--- scripts/extract_fluscape_antigenicmap.R
+++ scripts/extract_fluscape_antigenicmap.R
@@ -7,9 +7,13 @@ virus_key <- c("HK68"=1968, "EN72"=1972, "VI75"=1975, "TX77"=1977, "BK79"=1979, 
                "BE92"=1992, 
                "WU95"=1995, "SY97"=1997, "FU02"=2002, "CA04"=2004, "WI05"=2005, "PE06"=2006)
 
+## Following assumptions:
+## 1. X31 == 1969
+## 2. PE2009 is like the strain circulating in 2010
+
 fluscape_virus_key <- c("BJ89"=1989, "SC87"=1987, "PH82"=1982, "BR07"=2007, "WU95"=1995, "BK79"=1979, "HK14"=2014, "TX12"=2012, 
-  "PE09"=2009, "BJ92"=1992, "TX77"=1977, "VC09"=2009, "CL04"=2004, "VC98"=1998, "FJ00"=2000, "VC75"=1975, 
-  "MS85"=1985, "FJ02"=2002, "EN72"=1972, "X31"=1931, "HK68"=1968)
+  "PE09"=2010, "BJ92"=1992, "TX77"=1977, "VC09"=2009, "CL04"=2004, "VC98"=1998, "FJ00"=2000, "VC75"=1975, 
+  "MS85"=1985, "FJ02"=2002, "EN72"=1972, "X31"=1969, "HK68"=1968)
 
 
 antigenicMap$Strain <- virus_key[antigenicMap$Strain]
@@ -29,4 +33,4 @@ p1 <- ggplot(antigenicMap) +
     theme_bw()
 
 colnames(fit_dat) <- c("x_coord","y_coord","inf_years")
-#write.table(fit_dat,"data/fluscape_map.csv",row.names=FALSE,sep=",")
+write.table(fit_dat,"data/fluscape_map.csv",row.names=FALSE,sep=",")
