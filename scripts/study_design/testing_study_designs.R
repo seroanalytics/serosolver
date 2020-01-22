82c219ab875b34fe4116f80de6b7057273ea71ac
amanda-minter <amanda.minter@lshtm.ac.uk>
2020-01-22 15:00:39 +0000

    Update testing_study_designs.R

--- scripts/study_design/testing_study_designs.R
+++ scripts/study_design/testing_study_designs.R
@@ -3,9 +3,9 @@ setwd(code.dir)
 devtools::load_all()
 
 ## Specify paramters controlling the MCMC procedure
-mcmc_pars <- c("iterations"=500000,"adaptive_period"=500000, "burnin"=50000,
-               "hist_sample_prob"=0.5,"thin"=50,"thin_hist"=200,
-               "swap_propn"=0.5,"hist_switch_prob"=0.2,"year_swap_propn"=0.5)
+mcmc_pars <- c("iterations"=1000000,"adaptive_period"=7500000, "burnin"=500000,
+               "hist_sample_prob"=0.5,"thin"=50,"thin_hist"=200,"swap_propn"=0.5,
+               "hist_switch_prob"=0.2,"year_swap_propn"=0.5)
 
 
 ## Antigenic map for cross reactivity parameters
@@ -30,25 +30,34 @@ cl<-makeCluster(4)
 #register cluster
 registerDoParallel(cl)
 
-# ###mcmc chains
-# for(f in 1:3){
-#   foreach(i=1:3)%do%{
-#     for(type in c("L","CS")){
-#       filename <- paste0(i,"study",f,"_b_", type, sep="")
-#       
-#       titre_dat <- read.csv(paste("data/study_design/",filename,"dat.csv",sep = ""))
-#       ages <- read.csv(paste("data/study_design/",filename,"ages.csv",sep = ""))
-#       
-#      tryCatch( res <- run_MCMC(par_tab = par_tab, titre_dat = merge(titre_dat,ages, by = "individual"), 
-#                       antigenic_map = fit_dat, mcmc_pars = mcmc_pars,
-#                       mvr_pars = NULL, start_inf_hist = NULL, filename=paste("chains/", filename,sep=""),
-#                       CREATE_POSTERIOR_FUNC = create_posterior_func_fast, CREATE_PRIOR_FUNC = NULL,
-#                       version = 2,  
-#                       fast_version = TRUE),
-#       error = function(e) write.table(filename, paste(filename,".txt",sep="")))
-#     }
-#   }
-# }
+###mcmc chains
+for(f in 1:3){
+  foreach(i=1:3)%do%{
+    for(type in c("L","CS")){
+      filename <- paste0(i,"study",f,"_", type, sep="")
+
+      titre_dat <- read.csv(paste("data/study_design/",filename,"dat.csv",sep = ""))
+      ages <- read.csv(paste("data/study_design/",filename,"ages.csv",sep = ""))
+
+      
+      tryCatch( res <- run_MCMC(par_tab = par_tab, titre_dat = merge(titre_dat,ages, by = "individual"),
+                                antigenic_map = fit_dat, mcmc_pars = mcmc_pars,
+                                mvr_pars = NULL, start_inf_hist = NULL, filename=paste("chains/", filename,"_1_",sep=""),
+                                CREATE_POSTERIOR_FUNC = create_posterior_func_fast, CREATE_PRIOR_FUNC = NULL,
+                                version = 2,
+                                fast_version = TRUE),
+                error = function(e) write.table(filename, paste(filename,".txt",sep="")))
+      
+     tryCatch( res <- run_MCMC(par_tab = par_tab, titre_dat = merge(titre_dat,ages, by = "individual"),
+                      antigenic_map = fit_dat, mcmc_pars = mcmc_pars,
+                      mvr_pars = NULL, start_inf_hist = NULL, filename=paste("chains/", filename,"_2_",sep=""),
+                      CREATE_POSTERIOR_FUNC = create_posterior_func_fast, CREATE_PRIOR_FUNC = NULL,
+                      version = 2,
+                      fast_version = TRUE),
+      error = function(e) write.table(filename, paste(filename,".txt",sep="")))
+    }
+  }
+}
 
 
 
@@ -423,6 +432,7 @@ for(f in 1:2){
            ylab="Relative error",xlab = "", xaxt = "n", col = col_vec,
            pch = pch_vec, main = titles[k])
     abline(h=0, col = "gray", lty = "dashed")
+    abline(v = 3.5)
     k <- k +1
   }
 }
