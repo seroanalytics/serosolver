af77602eb7e88d58ced0720f1cdb239457e467c2
James Hay <9113187+jameshay218@users.noreply.github.com>
2024-04-25 09:34:10 +0100

    Updating vignette

--- inst/extdata/scripts/testing_longitudinal_with_start_levels.R
+++ inst/extdata/scripts/testing_longitudinal_with_start_levels.R
@@ -22,11 +22,13 @@ par_tab <- read.csv("~/Documents/GitHub/serosolver/inst/extdata/par_tab_base.csv
 
 possible_exposure_times <- seq(2000,2024,by=1)
 
+possible_exposure_times <- c(seq(2000,2020,by=4),seq(2021,2024,by=1))
+
 ## Vector of antigens that have biomarker measurements (note only one representative antigen per time)
 sampled_antigens <- max(possible_exposure_times)
 
 ## Times at which serum samples can be taken
-sampling_times <- 2010:2024
+sampling_times <- 2020:2024
 
 ## Number of serum samples taken
 n_samps <- 5
@@ -43,7 +45,7 @@ all_simulated_data <- simulate_data(par_tab=par_tab, group=1, n_indiv=50,
                                     possible_exposure_times=possible_exposure_times,
                                     measured_biomarker_ids = sampled_antigens,
                                     sampling_times=sampling_times, nsamps=n_samps,
-                                    antigenic_map=antigenic_map,
+                                    antigenic_map=NULL,
                                     age_min=10,age_max=75,
                                     attack_rates=attack_rates, repeats=1,
                                     data_type=c(2))
@@ -71,9 +73,9 @@ par_tab[par_tab$names == "boost_long","values"] <- 0
 par_tab[par_tab$names == "boost_long","fixed"] <- 1
 antibody_data1 <- antibody_data %>% group_by(individual) %>% mutate(birth=min(sample_time)) %>% as.data.frame()
 res <- serosolver(par_tab, antibody_data1, NULL,
-                  possible_exposure_times=2010:max(possible_exposure_times),
+                  possible_exposure_times=possible_exposure_times,#2010:max(possible_exposure_times),
                   filename="test_long/readme", prior_version=2,n_chains=1,parallel=FALSE,
-                  mcmc_pars=c(adaptive_iterations=20000, iterations=50000,proposal_ratio=2),verbose=TRUE,
+                  mcmc_pars=c(adaptive_iterations=20000, iterations=50000,proposal_ratio=2),verbose=TRUE,verbose_dev = TRUE,
                   data_type=2,
                   start_level="mean")
 chains <- load_mcmc_chains(location="test_long",par_tab=par_tab,burnin = 20000,unfixed=FALSE)
