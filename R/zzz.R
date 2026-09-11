d53920898136de30364c9d5bfb9d30e36536afc7
James Hay <9113187+jameshay218@users.noreply.github.com>
2026-09-11 19:45:57 +0100

    Trying to correct namespace issues

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,16 @@
+# Modified by an AI assistant on 2026-09-11 using GPT-5. Added package-level
+# roxygen imports for functions used from imported dependencies; no functional
+# code was changed.
 .datatable.aware <- TRUE
 #' @useDynLib serosolver
 #' @importFrom Rcpp evalCpp
+#' @importFrom data.table `%like%` data.table fsetdiff key
+#' @importFrom dplyr `%>%` across all_of arrange bind_cols bind_rows
+#'   case_when cur_group_id distinct everything filter group_by if_else
+#'   left_join mutate n pull rename row_number sample_n select summarise
+#'   summarize tally tibble ungroup
+#' @importFrom tidyr complete drop_na expand_grid pivot_longer pivot_wider
+#'   unnest
+#' @importFrom coda as.mcmc as.mcmc.list effectiveSize gelman.diag
+#' @importFrom foreach foreach `%do%`
 NULL
